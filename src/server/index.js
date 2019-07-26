529bc5b3095818b9d0602e63fff64727e1612317
abidex4yemi <abidex4yemi@gmail.com>
2019-07-26 12:24:40 +0100

    Create application main file (/server/index.js)

---
+++ src/server/index.js
@@ -0,0 +1,37 @@
+/**
+ * Application Main file
+ */
+const express = require('express');
+const logger = require('morgan');
+const dotenv = require('dotenv');
+const helmet = require('helmet');
+const cors = require('cors');
+
+const allErrorHandler = require('./middleware/errors');
+const { createSuccess, OK } = require('./util/success');
+const { NOT_FOUND } = require('./util/error');
+
+const app = express();
+
+dotenv.config();
+
+/**
+ * Set up middleware
+ */
+app.use(express.json());
+app.use(cors());
+app.use(logger('dev'));
+app.use(helmet());
+
+app.get('/', (req, res) => res.status(OK).json(createSuccess({ message: 'API is alive...', data: [] })));
+
+// Handle invalid request
+app.all('*', (req, res) => res.status(NOT_FOUND).json({
+  success: false,
+  message: 'Route does not exist...',
+  body: [],
+}));
+
+app.use(allErrorHandler());
+
+module.exports = app;
